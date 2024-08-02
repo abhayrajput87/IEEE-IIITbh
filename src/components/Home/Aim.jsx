434aae1b6446a32fc1a1d5febf7deb2e159b1e29
abhayrajput87 <itzrajputabhay@gmail.com>
2024-08-02 15:26:23 +0530

    date updated

--- src/components/Home/Aim.jsx
+++ src/components/Home/Aim.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import touch from "../../assets/Navbar/touch.jpg";
+import Important from "./Important";
 
 function Aim() {
   return (
@@ -20,28 +21,7 @@ function Aim() {
         <div className="md:w-1/2 mx-auto text-black px-8 mb-8 ">
           <h1 className="font-bold text-4xl mb-4 ">Important Dates</h1>
           <div className="border-2 rounded-lg font-sans p-8  border-gray-400">
-            <ul className="list-disc font-semibold space-y-2">
-              <li>
-                Paper submission starts:
-                <span className=" font-bold">20th December 2023</span>
-              </li>
-              <li>
-                Date of conference:
-                <span className=" font-bold">19th to 21st December 2024</span>
-              </li>
-              <li>
-                Paper submission deadline:
-                <span className=" font-bold"> 1st August 2024</span>
-              </li>
-              <li>
-                Paper acceptance notification:
-                <span className=" font-bold"> 30th September 2024</span>
-              </li>
-              <li>
-                Camera ready submission and registration deadline:
-                <span className=" font-bold">31st October 2024</span>
-              </li>
-            </ul>
+            <Important />
           </div>
         </div>
       </div>
